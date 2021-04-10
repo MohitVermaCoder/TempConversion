3677b84e3ba27bcb01a2fffbce918a1611851f9c
MohitVermaCoder <verma.mohit95@outlook.com>
2021-04-10 13:49:43 +0530

    temp conversion

---
+++ src/app/temp-convert/temp-convert.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
+
+@Component({
+  selector: 'app-temp-convert',
+  templateUrl: './temp-convert.component.html',
+  styleUrls: ['./temp-convert.component.scss']
+})
+export class TempConvertComponent implements OnInit {
+
+  temperatureConverter: FormGroup;
+  result:any;
+
+  constructor() { }
+
+  ngOnInit() {
+
+    this.temperatureConverter= new FormGroup({
+      celsius: new FormControl(),
+      fahren: new FormControl()
+    })
+  }
+
+  celToFah(data:any){
+    // F = C*9/5 + 32
+console.log(data);
+this.result=data.celsius*9/5+32;
+this.temperatureConverter.controls.fahren.setValue(this.result);
+
+  }
+
+  fahToCel(data:any){
+    // C = (F − 32) × 5/9
+    this.result=(data.fahren-32)*5/9;
+    this.temperatureConverter.controls.celsius.setValue(this.result);
+  }
+
+}
